7175d19f3c3fbc3944280dd94a7612b282f65317
AmiraEltaher <amiraeltaher2020@gmail.com>
2024-01-27 00:25:50 +0200

    login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('userList', [UserController::class, 'create'])->name('userList');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
